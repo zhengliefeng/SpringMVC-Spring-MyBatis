d1bfaa92ef0cea8ceb07e942facffc5d1d22f450
zhengliefeng <1335729895@qq.com>
2016-05-24 08:17:32 +0800

    切换数据库文件

---
+++ src/main/java/rml/utl/JUnit4ClassRunner.java
@@ -0,0 +1,25 @@
+package rml.utl;
+
+import java.io.FileNotFoundException;
+
+import org.junit.runners.model.InitializationError;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Log4jConfigurer;
+
+public class JUnit4ClassRunner extends SpringJUnit4ClassRunner {
+
+	static {
+		try {
+			Log4jConfigurer.initLogging("src/main/resources/log4j.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public JUnit4ClassRunner(Class<?> clazz) throws InitializationError {
+		super(clazz);
+		// TODO Auto-generated constructor stub
+	}
+
+}
